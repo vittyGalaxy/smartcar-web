22982a409a0e77615e3d2edd8eeb730160cb2e6f
Vittorione <vittorio.tiozzo.dev@gmail.com>
2026-05-03 20:37:28 +0200

    implemented index.php

---
+++ src/index.php
@@ -0,0 +1,10 @@
+<html>
+<title> Aserial Command </title>
+    <head>
+        <h1> Aserial command </h1>
+    </head>
+    <body>
+        <a href="site_web/button_move.html"> Aserial Move Control </a>    
+    <br>
+    </body>
+</html>
