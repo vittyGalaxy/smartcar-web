e4d00479d713a1c72ef183a409ce591d42b9d04c
Vittorione <vittorio.tiozzo.dev@gmail.com>
2026-05-06 19:37:24 +0200

    added image smartcar

--- src/index.php
+++ src/index.php
@@ -4,6 +4,8 @@
         <h1> Aserial command </h1>
     </head>
     <body>
+        <img src="../img/aserial.jpg">
+        <br>
         <a href="site_web/button_move.html"> Aserial Move Control </a>    
     <br>
     </body>
