02149d0723c3c653856926153da43a6c1dea95b4
Vittorione <vittorio.tiozzo.dev@gmail.com>
2026-06-02 19:52:00 +0200

    populate table Control

---
+++ sql/populate_control.sql
@@ -0,0 +1,6 @@
+INSERT INTO Control(name, description) VALUES
+    ("Forward",     "This control is used to make the smartcar walk in front of him"),
+    ("Right",       "This control is used to make the smartcar turn to the right"),
+    ("Backward",    "This control is used to make the smartcar walk behind him"),
+    ("Left",        "This control is used to make the smartcar turn to the left"),
+    ("Stop",        "This control is used to stop the smart car in case of safety")
